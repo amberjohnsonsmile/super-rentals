96e309e16c6c1016e2202160010bdfaf862ad7ac
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-01-18 15:31:39 -0700

    Initial commit

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,22 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list rentals');
+
+test('should show rentals as the home page.', (assert) => {
+});
+
+test('should link to information about the company.', (assert) => {
+});
+
+test('should link to contact information.', (assert) => {
+});
+
+test('should list available rentals.', (assert) => {
+});
+
+test('should filter the list of rentals by city.', (assert) => {
+});
+
+test('should show details for a selected rental', (assert) => {
+});
